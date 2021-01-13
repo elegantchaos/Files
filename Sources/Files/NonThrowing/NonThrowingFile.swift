36b4d04e709799b728973d5e3fb62d8f350e9c53
Sam Deane <sam@elegantchaos.com>
2021-01-13 15:07:12 +0000

    added asData/write(asData

--- Sources/Files/NonThrowing/NonThrowingFile.swift
+++ Sources/Files/NonThrowing/NonThrowingFile.swift
@@ -9,8 +9,15 @@ public struct NonThrowingFile: NonThrowingItem {
     public let ref: NonThrowingReference
     public var isFile: Bool { true }
     public typealias Manager = NonThrowingManager
-    
+
+    public var asText: String? { try? String(contentsOf: ref.url, encoding: .utf8) }
+    public var asData: Data? { try? Data(contentsOf: ref.url) }
+
     public init(ref: NonThrowingReference) {
         self.ref = ref
     }
+
+    public func write(asText text: String) { try? text.write(to: ref.url, atomically: true, encoding: .utf8) }
+    public func write(asData data: Data) { try? data.write(to: ref.url) }
+
 }
--- Sources/Files/Throwing/ThrowingFile.swift
+++ Sources/Files/Throwing/ThrowingFile.swift
@@ -11,10 +11,13 @@ public struct ThrowingFile: ThrowingItem {
     public let ref: ThrowingReference
     public var isFile: Bool { true }
     public var asText: String? { try? String(contentsOf: ref.url, encoding: .utf8) }
+    public var asData: Data? { try? Data(contentsOf: ref.url) }
     
     public init(ref: ThrowingReference) {
         self.ref = ref
     }
     
-    public func write(as text: String) { try? text.write(to: ref.url, atomically: true, encoding: .utf8) }
+    public func write(as text: String) { try? text.write(to: ref.url, atomically: true, encoding: .utf8) } // deprecated
+    public func write(asText text: String) { try? text.write(to: ref.url, atomically: true, encoding: .utf8) }
+    public func write(asData data: Data) { try? data.write(to: ref.url) }
 }
